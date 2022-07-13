a916337003516e41cd671ac3eb9af0a747dba343
LTFYR <lutfiyariea@code.edu.az>
2022-07-13 10:19:09 +0400

    last

---
+++ UpConstructor/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using UpConstructor.DAL;
+using UpConstructor.Models;
+
+namespace UpConstructor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Slider> slider = _context.Sliders.ToList();
+            return View(slider);
+        }
+
+    }
+}
